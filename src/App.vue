a036b920d13d521490fbfac01bdf25756084641a
Miguel Santos <enium.miguel@gmail.com>
2024-01-03 15:04:52 -0300

    Super Update

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <v-card>
-    <v-card-text>
-      <v-row>
-        
-        <v-col>
-          <v-text-field label="Nome completo"></v-text-field>
-        </v-col>
+  <v-app  theme="dark">
+    <v-app-bar flat class="border-b">
+      <v-app-bar-title>Ceyp Social</v-app-bar-title>
+    </v-app-bar>
+    
+  <v-main>
+    <v-container>
+        <v-card>
+         <h1 class="pa-4 text-brand">Cadastro</h1>
+         <v-card-text>
 
-        <v-col>
-          <v-text-field
-          label="Data de Nascimento"
-          placeholder="00/00/0000"
-          ></v-text-field>
-        </v-col>
+                <v-text-field label="Nome completo" variant="outlined" placeholder="Seu nome"></v-text-field>
+ 
+                <v-text-field label="Data de nascimento" variant="outlined" placeholder="00/00/0000"></v-text-field>
 
-          <v-radio-group>
-            <v-radio label="Masculino" value="M"></v-radio>
-            <v-radio label="Feminino" value="F"></v-radio>
-          </v-radio-group>
+                <v-select label="Sexo" variant="outlined" :items="['Masculino', 'Feminino']">
+                </v-select>
 
-          <v-combobox
-            label="Cor"
-            :items="['Branco', 'Pardo', 'Negro']">
-          </v-combobox>
+                <v-text-field
+              :counter="9"
+              label="Telefone"
+              variant="outlined"
+              placeholder="(00) 00000000">
+                </v-text-field>
+                <v-checkbox class="" label="Whatsapp"></v-checkbox>
+              <v-select
+                label="Cor"
+                variant="outlined"
+                :items="['Branco', 'Pardo', 'Negro']">
+              </v-select>
+              <v-text-field
+                label="Endereço"
+                variant="outlined"
+                placeholder="Bairro, lagradouro, número e referência">
+              </v-text-field>
+              <v-btn class="w-100 w-max-width" variant="tonal">Cadastrar</v-btn>
 
-      </v-row>
-    </v-card-text>
-  </v-card>
+          </v-card-text>
+      </v-card>
+    </v-container>
+  </v-main>
+  </v-app>
 
 </template>
 
