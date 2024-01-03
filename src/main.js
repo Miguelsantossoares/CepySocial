a34fbf284a4d8ab259ad43999b1da20d74edfb28
Miguel Santos <enium.miguel@gmail.com>
2024-01-03 17:46:28 -0300

    DownDate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { registerPlugins } from '@/plugins';
 import { createPinia } from 'pinia';
-import App from './App.vue';
+import App from './App.vue'
 
 const app = createApp(App);
 app.use(createPinia());
